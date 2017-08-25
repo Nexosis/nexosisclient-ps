d22462cb5683858a0f551049ec63f7243d78c5f7
Jason D. Montgomery <jason@nexosis.com>
2017-08-25 16:58:48 -0400

    Last test of deploy for AppVeyor - make intentionally broken test work again.

--- PSNexosisClient/tests/Unit/Get-AccountBalance.tests.ps1
+++ PSNexosisClient/tests/Unit/Get-AccountBalance.tests.ps1
@@ -36,7 +36,7 @@ Describe "Get-AccountBalance" -Tag 'Unit' {
 		It "gets account balance" {
 			$value = Get-AccountBalance
 			Assert-MockCalled Invoke-WebRequest -ModuleName PSNexosisClient -Times 1 -Scope It
-			$value | should be "200 USDAAA"
+			$value | should be "200 USD"
 		}
         
 		It "uses the mock" {
