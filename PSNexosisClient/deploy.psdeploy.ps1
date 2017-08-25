3bb45030d85a1e03981174e72743c69afc10b4a4
Jason D. Montgomery <jason@nexosis.com>
2017-08-25 15:52:13 -0400

    commented out bottom of deploy script... !deploy

--- PSNexosisClient/deploy.psdeploy.ps1
+++ PSNexosisClient/deploy.psdeploy.ps1
@@ -45,18 +45,18 @@ else
 }
 
 # Publish to AppVeyor if we're in AppVeyor
-if(
-    $env:BHPSModulePath -and
-    $env:BHBuildSystem -eq 'AppVeyor'
-   )
-{
-    Deploy DeveloperBuild {
-        By AppVeyorModule {
-            FromSource $ENV:BHPSModulePath
-            To AppVeyor
-            WithOptions @{
-                Version = $env:APPVEYOR_BUILD_VERSION
-            }
-        }
-    }
-}
+#if(
+#    $env:BHPSModulePath -and
+#    $env:BHBuildSystem -eq 'AppVeyor'
+#   )
+#{
+#    Deploy DeveloperBuild {
+#        By AppVeyorModule {
+#            FromSource $ENV:BHPSModulePath
+#            To AppVeyor
+#            WithOptions @{
+#                Version = $env:APPVEYOR_BUILD_VERSION
+#            }
+#        }
+#    } -Verbose
+#}
