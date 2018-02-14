62f7fad753ca5f9e5043b7c5b8cbf307465f8025
Jason D. Montgomery <jason@nexosis.com>
2018-02-14 18:26:46 -0500

    Added DataSetStatistic endpoint and unit tests.

---
+++ PSNexosisClient/PSNexosisClient/Public/Get-NexosisDataSetStatistics.ps1
@@ -0,0 +1,30 @@
+Function Get-NexosisDataSetStatistics {
+<# 
+ .Synopsis
+  Gets the column stats for the given data source.
+
+ .Description
+  Returns statistics on each column in a dataset such as row count, min, max, mean, median, stddev, variance, count of non numeric values, and errors.
+ 
+ .Parameter dataSetName
+  The name of the data set for which to retrieve column stats.
+
+ .Example
+  # Return the Statistics from the DataSet named salesdata
+  Get-NexosisDataSetStatistics -DataSetName salesdata
+#>[CmdletBinding()]
+	Param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$True)]
+        [string]$dataSetName
+	)
+    process {
+        $result = Invoke-Http -method Get -path "data/$dataSetName/stats"
+        $statistics = @()
+        foreach ($column in $result.columns) {
+            $entry = @{}
+            $entry.Add(($column | Get-Member)[-1].Name, $column.(($column | Get-Member)[-1].Name))
+            $statistics += $entry
+        }
+        $statistics
+    }
+}
---
+++ PSNexosisClient/tests/Unit/Get-DataSetStatistics.tests.ps1
@@ -0,0 +1,81 @@
+# For build automation to enable verbose
+if($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "!verbose")
+{
+	$VerbosePreference = "continue"
+}
+
+Remove-Module PSNexosisClient -ErrorAction SilentlyContinue
+Import-Module "$PSScriptRoot\..\..\PSNexosisClient"
+
+$PSVersion = $PSVersionTable.PSVersion.Major
+
+Describe "Get-NexosisDataSetStatistics" -Tag 'Unit' {
+	Context "unit tests" {
+        Set-StrictMode -Version latest		
+
+		BeforeAll {
+			$moduleVersion = (Test-ModuleManifest -Path $PSScriptRoot\..\..\PSNexosisClient\PSNexosisClient.psd1).Version
+            $TestVars = @{
+                ApiKey       = $Env:NEXOSIS_API_KEY
+				UserAgent	 = "Nexosis-PS-API-Client/$moduleVersion"
+				ApiEndPoint	 = $Env:NEXOSIS_API_TESTURI
+				MaxPageSize  = "1000"
+            }
+        }
+		
+		Mock -ModuleName PSNexosisClient Invoke-WebRequest { 
+			param($Uri, $Method, $Headers, $ContentType, $Body, $InFile)
+            $response =  New-Object PSObject -Property @{
+				StatusCode="200"
+				Headers=@{}
+				Content=''
+			}
+			if($Headers['accept'] -eq 'application/json') {
+				$response.Content = "{ }"
+			} elseif ($Headers['accept'] -eq 'text/csv') {
+				$response.Content = "A,B,C,D`r`n1,2,3,4`r`n"
+			}
+			$response
+        } -Verifiable
+        
+        It "gets dataset statistics by dataset name" {
+            Get-NexosisDataSetStatistics -dataSetName 'salesdata'
+            Assert-MockCalled Invoke-WebRequest -ModuleName PSNexosisClient -Times 1 -Scope It
+        }
+
+		It "uses the mock" {
+			Assert-VerifiableMock
+        }
+		
+		It "calls with the proper URI" {
+			Assert-MockCalled Invoke-WebRequest -ModuleName PSNexosisClient -Times 1 -Scope Context -ParameterFilter {
+				$Uri -eq "$($TestVars.ApiEndPoint)/data/salesdata/stats"
+			} 
+		}
+
+        It "calls with the proper HTTP verb" {
+			Assert-MockCalled Invoke-WebRequest -ModuleName PSNexosisClient -Times 1 -Scope Context -ParameterFilter {
+				$method -eq [Microsoft.PowerShell.Commands.WebRequestMethod]::Get
+			}
+        }
+
+         It "calls with the proper content-type" {
+			Assert-MockCalled Invoke-WebRequest -ModuleName PSNexosisClient -Times 1 -Scope Context -ParameterFilter {
+				$ContentType -eq 'application/json'
+			}
+        }
+
+        It "has proper HTTP headers" {
+			Assert-MockCalled Invoke-WebRequest -ModuleName PSNexosisClient -Times 1 -Scope Context -ParameterFilter {
+				(
+					($Headers.Contains("accept")) -and 
+					($Headers.Contains("api-key")) -and
+					($Headers.Contains("User-Agent")) -and
+					($Headers.Get_Item("accept") -eq 'application/json') -and
+					($Headers.Get_Item("api-key") -eq $TestVars.ApiKey) -and
+					($Headers.Get_Item("User-Agent") -eq $TestVars.UserAgent)
+				)
+			}
+		}
+	}
+}
